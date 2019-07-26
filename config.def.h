0f55d15366b7b4a9921c734f92a30252fc77af48
Uriah Marc Todoroff <aeymxq@gmail.com>
2019-07-26 14:44:18 -0400

    battery

--- config.def.h
+++ config.def.h
@@ -63,7 +63,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ battery_state, ,  "BAT0"   },
-	{ battery_perc, ,  "BAT0"   },
+	{ battery_state,   "BAT0"   },
+	{ battery_perc,   "BAT0"   },
 	{ datetime, "%s",           "%F %T" },
 };
